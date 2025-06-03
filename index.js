3593fa428df32a01e3d484dfb18dd72b7254e21b
Alaa Abdullah <alaaabdullh43@gmail.com>
2025-06-03 22:24:01 +0300

    added here error handel

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import initializeApp from "./src/bootstrap.js";
+import employeeRoutes from './src/modules/employee/employee.router.js'
 
 dotenv.config({ path: "./config/.env" });
+
 const port = process.env.PORT || 5000;
 const app = express();
 initializeApp(app, express);
+
+app.use('/api/employees', employeeRoutes);
+
+
+
+app.use("/{*any}", (req, res, next) => {
+    res.status(404).json({
+      success: false,
+      message: `Can't find this route: ${req.originalUrl}`,
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Server running successfully on port ${port}`);
