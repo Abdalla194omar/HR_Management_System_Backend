30402edd55c2228c5e2dbd24780eed048d39a45d
Alaa Abdullah <alaaabdullh43@gmail.com>
2025-06-04 00:42:16 +0300

    add get employeebyId

--- src/modules/employee/controller/employee.controller.js
+++ src/modules/employee/controller/employee.controller.js
@@ -24,3 +24,19 @@ export const getAllEmployees =async(req,res)=>{
   }
 
 };
+
+// get employee by id 
+export const getEmployeeByid =async(req,res)=>{
+  try {
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+      return res.status(404).json({message:"Error employee not found"});
+    }
+    res.status(201).json(employee);
+    
+  } catch (error) {
+        res.status(500).json({ message: error.message });
+
+  }
+
+};
