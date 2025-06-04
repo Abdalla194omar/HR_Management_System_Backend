410191b5a2f278093c346e3d4049f7d0d60c992e
ffattma <fa2325285@gmail.com>
2025-06-04 06:21:04 +0300

    Updated code with bug fixes

--- src/modules/department/controller/department.controller.js
+++ src/modules/department/controller/department.controller.js
@@ -1,57 +1,3 @@
-// const Department = require('../../../../DB/model/Department.js');
-
-// // Create
-// exports.createDepartment = async (req, res) => {
-//   try {
-//     const { name, description } = req.body;
-//     const department = await Department.create({ name, description });
-//     res.status(201).json(department);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// // Read
-// exports.getDepartments = async (req, res) => {
-//   try {
-//     const departments = await Department.findAll();
-//     res.status(200).json(departments);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// // Update
-// exports.updateDepartment = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { name, description } = req.body;
-//     const department = await Department.findByPk(id);
-//     if (!department) return res.status(404).json({ message: 'Not found' });
-
-//     department.name = name;
-//     department.description = description;
-//     await department.save();
-
-//     res.json(department);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// // Delete
-// exports.deleteDepartment = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const department = await Department.findByPk(id);
-//     if (!department) return res.status(404).json({ message: 'Not found' });
-
-//     await department.destroy();
-//     res.json({ message: 'Deleted successfully' });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
 import Department from "../../../../DB/model/Department.js";
 
 // Create department
@@ -81,3 +27,5 @@ export const getAllDepartments = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
