a83c98ebe9b62a41a8a9fcf112575c684ff4efc7
ayasalah600 <you@example.com>
2025-06-05 12:48:36 +0300

    validation

--- src/modules/holiday/holiday.router.js
+++ src/modules/holiday/holiday.router.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { validateHoliday } from "./holiday.validation.js";
+
 import { createHoliday } from "./controller/holiday.controller.js";
 
 const router = express.Router();
 
-router.post("/", validateHoliday, createHoliday);
+router.post("/", createHoliday);
 
 export default router;
--- src/modules/holiday/holiday.validation.js
+++ src/modules/holiday/holiday.validation.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import validation from "../../middleWare/validation.js";
+
 
 const holidaySchema = {
   body: Joi.object({
