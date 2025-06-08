b2e3f0d4fe3e2d0f7ce65e336826c34422dae871
yasminzin <yasminaymanzin@gmail.com>
2025-06-08 22:27:46 +0300

    edit validations

--- src/modules/attendance/attendance.validation.js
+++ src/modules/attendance/attendance.validation.js
@@ -35,29 +35,15 @@ export const createCheckIneSchema = {
         "string.pattern.base": "Check-in time must be in HH:MM 24-hour format",
         "any.required": "Check-in time is required when status is Present",
       }),
-    status: joi
-      .string()
-      .valid("Present", "Absent")
-      .default("Present")
-      .messages({
-        "any.only": "Status must be one of Present or Absent",
-        "string.base": "Status must be a string",
-      }),
+    status: joi.string().valid("Present", "Absent").default("Present").required().messages({
+      "any.only": "Status must be one of Present or Absent",
+      "string.base": "Status must be a string",
+    }),
   }),
 };
 
 export const createCheckOutSchema = {
   body: joi.object({
-    employee: joi.string().required().length(24).hex().messages({
-      "string.base": "Employee ID must be a string",
-      "string.length": "Employee ID must be exactly 24 characters long",
-      "string.hex": "Employee ID must be a valid hexadecimal string",
-      "any.required": "Employee ID is required",
-    }),
-    date: joi.date().required().messages({
-      "date.base": "Date must be a valid date",
-      "any.required": "Date is required",
-    }),
     checkOutTime: joi.string().pattern(timePattern).required().messages({
       "string.pattern.base": "Check-out time must be in HH:MM 24-hour format",
       "any.required": "Check-out time is required",
@@ -101,36 +87,25 @@ export const createAttendanceSchema = {
         "string.pattern.base": "Check-out time must be in HH:MM 24-hour format",
         "any.required": "Check-out time is required when status is Present",
       }),
-    status: joi
-      .string()
-      .valid("Present", "Absent")
-      .default("Present")
-      .messages({
-        "any.only": "Status must be one of Present or Absent",
-        "string.base": "Status must be a string",
-      }),
+    status: joi.string().valid("Present", "Absent").default("Present").required().messages({
+      "any.only": "Status must be one of Present or Absent",
+      "string.base": "Status must be a string",
+    }),
   }),
 };
 
 export const updateAttendanceSchema = {
   body: joi.object({
-    date: joi.date().messages({
-      "date.base": "Date must be a valid date",
-    }),
     checkInTime: joi.string().pattern(timePattern).messages({
       "string.pattern.base": "Check-in time must be in HH:MM 24-hour format",
     }),
     checkOutTime: joi.string().pattern(timePattern).allow("").messages({
       "string.pattern.base": "Check-out time must be in HH:MM 24-hour format",
     }),
-    status: joi
-      .string()
-      .valid("Present", "Absent")
-      .default("Present")
-      .messages({
-        "any.only": "Status must be one of Present or Absent",
-        "string.base": "Status must be a string",
-      }),
+    status: joi.string().valid("Present", "Absent").default("Present").required().messages({
+      "any.only": "Status must be one of Present or Absent",
+      "string.base": "Status must be a string",
+    }),
   }),
   params: joi.object({
     id: joi.string().alphanum().length(24).required().messages({
