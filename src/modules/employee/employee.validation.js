414080da534a89b47d9f8cb60199adc9429df758
Alaa Abdullah <alaaabdullh43@gmail.com>
2025-06-10 23:55:17 +0300

    update  employee creation validation schema and  Add employee upate validation schema using Joi

--- src/modules/employee/employee.validation.js
+++ src/modules/employee/employee.validation.js
@@ -25,7 +25,7 @@ export const createEmployeeSchema = {
         "string.empty": "Phone number is required",
         "any.required": "Phone number is required",
       }),
-    department: Joi.string().required().messages({
+    department: Joi.string().length(24).hex().required().messages({
         "string.length": "Department ID must be 24 characters",
         "string.hex": "Department ID must be a valid hexadecimal string",
         "any.required": "Department ID is required",
@@ -63,6 +63,88 @@ export const createEmployeeSchema = {
         "string.pattern.base": "National ID must be 14 digits",
         "string.empty": "National ID is required",
         "any.required": "National ID is required",
+      }),
+    weekendDays: Joi.array().items(
+      Joi.string().valid("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    ).default(["Friday", "Saturday"]),
+    overtimeType: Joi.string().valid("hour", "Bound").default("hour"),
+    overtimeValue: Joi.number().min(0).default(0),
+    deductionType: Joi.string().valid("hour", "Bound").default("hour"),
+    deductionValue: Joi.number().min(0).default(0)
+  })
+}
+
+
+export const updateEmployeeSchema = {
+  params: Joi.object({
+    id: Joi.string()
+      .length(24)
+      .hex()
+      .required()
+      .messages({
+        "string.length": "ID must be 24 characters",
+        "string.hex": "ID must be a valid hexadecimal string",
+        "any.required": "ID is required",
+      }),
+  }),
+  body: Joi.object({
+    firstName: Joi.string().trim().min(3).max(50).messages({
+    "string.empty":"First name is required",
+    "string.min": "First name must be at least 3 characters",
+    "string.max": "First name  must not exceed 50 characters",
+    }),
+    lastName: Joi.string().trim().min(3).max(50).messages({
+    "string.empty":"last name is required",
+    "string.min": "last name must be at least 3 characters",
+    "string.max": "last name  must not exceed 50 characters",
+    }),
+    email: Joi.string().email().messages({
+        "string.email": "Please enter a valid email address",
+        "string.empty": "Email is required",
+       
+      }),
+    phone: Joi.string().pattern(/^\+?\d{10,15}$/).required().messages({
+        "string.pattern.base": "Please enter a valid phone number (11 digits)",
+        "string.empty": "Phone number is required",
+       
+      }),
+    department: Joi.string().length(24).hex().messages({
+        "string.length": "Department ID must be 24 characters",
+        "string.hex": "Department ID must be a valid hexadecimal string",
+    
+      }),
+    hireDate: Joi.date().less('now').min("2008-01-01").messages({
+        "date.base": "Please enter a valid hire date",
+        "date.less": "Hire date cannot be in the future",
+        "date.min": "Hire date must not be before 2008",
+      
+      }),
+    salary: Joi.number().min(0)
+      .messages({
+        "number.base": "Please enter a valid salary",
+        "number.min": "Salary cannot be negative",
+      }),
+    workingHoursPerDay: Joi.number().min(1).max(24).default(8).messages({
+        "number.base": "Please enter a valid number of working hours",
+        "number.min": "Working hours must be at least 1",
+        "number.max": "Working hours must not exceed 24",
+      }),
+    defaultCheckInTime: Joi.string().pattern(/^([01]\d|2[0-3]):[0-5]\d(:[0-5]\d)?$/),
+    defaultCheckOutTime: Joi.string().pattern(/^([01]\d|2[0-3]):[0-5]\d(:[0-5]\d)?$/),
+    address: Joi.string().max(200).messages({
+        "string.max": "Address must not exceed 200 characters",
+      }),
+    gender: Joi.string().valid("Male", "Female"),
+    nationality: Joi.string().max(100),
+    birthdate: Joi.date().less('now').max(new Date(new Date().setFullYear(new Date().getFullYear() - 20))).messages({
+        "date.base": "Please enter a valid birthdate",
+        "date.max": "Employee must be at least 20 years old",
+        
+      }),
+    nationalId: Joi.string().pattern(/^\d{14}$/).messages({
+        "string.pattern.base": "National ID must be 14 digits",
+        "string.empty": "National ID is required",
+      
       }),
     weekendDays: Joi.array().items(
       Joi.string().valid("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
