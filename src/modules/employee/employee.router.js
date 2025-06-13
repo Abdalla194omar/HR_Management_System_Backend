0925293b1fb227dabb6599b3e55bf5d6bba13719
Alaa Abdullah <alaaabdullh43@gmail.com>
2025-06-13 03:28:22 +0300

    Refactor imports Use * as  imports for controllers and schemas

--- src/modules/employee/employee.router.js
+++ src/modules/employee/employee.router.js
@@ -1,38 +1,30 @@
 import express from "express";
-import {
-  createEmployee,
-  getAllEmployees,
-  getEmployeeByid,
-  SearchEmployee,
-  updateEmployee,
-  deleteEmployee,
-  getEmployeesFilter
-} from "./controller/employee.controller.js";
+import * as controllers from "./controller/employee.controller.js";
 import validation from "../../middleWare/validation.js";
-import {createEmployeeSchema,updateEmployeeSchema,getEmployeesFilterSchema} from './employee.validation.js'
+import * as schemas from './employee.validation.js'
 
 const router = express.Router();
 
 // create employee
-router.post("/",validation(createEmployeeSchema),createEmployee); // POST /api/employees/
+router.post("/",validation(schemas.createEmployeeSchema),controllers.createEmployee); // POST /api/employees/
 
 // get all
-router.get("/all", getAllEmployees);
+router.get("/all", controllers.getAllEmployees);
 
 // get all by filter 
-router.get("/", validation(getEmployeesFilterSchema),getEmployeesFilter);
+router.get("/", validation(schemas.getEmployeesFilterSchema),controllers.getEmployeesFilter);
 
 
 //search
-router.get("/search", SearchEmployee);
+router.get("/search", controllers.SearchEmployee);
 
 // get by id
-router.get("/:id", getEmployeeByid);
+router.get("/:id", controllers.getEmployeeByid);
 
 // update
-router.put("/:id",validation(updateEmployeeSchema), updateEmployee);
+router.put("/:id",validation(schemas.updateEmployeeSchema), controllers.updateEmployee);
 
 // delete
-router.delete("/:id", deleteEmployee);
+router.delete("/:id", controllers.deleteEmployee);
 
 export default router;
