8b89ba36cdbcf58fe61ce66cd50910ffaf1ee636
yasminzin <yasminaymanzin@gmail.com>
2025-06-24 21:10:22 +0300

    enhance routes

--- src/modules/attendance/attendance.router.js
+++ src/modules/attendance/attendance.router.js
@@ -5,18 +5,20 @@ import * as schemas from "./attendance.validation.js";
 
 export const router = express.Router();
 
-router.get("/", validation(schemas.getAttendanceSchema), controllers.getAttendance);
+router
+  .route("/")
+  .get(validation(schemas.getAttendanceSchema), controllers.getAttendance)
+  .post(validation(schemas.createAttendanceSchema), controllers.createAttendance);
+
+router
+  .route("/:id")
+  .patch(validation(schemas.updateAttendanceSchema), controllers.updateAttendance)
+  .delete(validation(schemas.deleteAttendanceSchema), controllers.deleteAttendance);
+
+router.post("/checkin", validation(schemas.createCheckIneSchema), controllers.createCheckIn);
 
 router.get("/absence", controllers.getTodayAbsence);
 
 router.get("/graph", controllers.getAttendanceGraph);
 
-router.post("/", validation(schemas.createAttendanceSchema), controllers.createAttendance);
-
-router.post("/checkin", validation(schemas.createCheckIneSchema), controllers.createCheckIn);
-
 router.patch("/checkout/:id", validation(schemas.createCheckOutSchema), controllers.createCheckOut);
-
-router.patch("/:id", validation(schemas.updateAttendanceSchema), controllers.updateAttendance);
-
-router.delete("/:id", validation(schemas.deleteAttendanceSchema), controllers.deleteAttendance);
