745bb2166ac80ea1cfc8aa56f1cf6f5e9ed51297
ayasalah600 <you@example.com>
2025-06-24 21:08:15 +0300

    login_holiday

--- src/modules/holiday/holiday.router.js
+++ src/modules/holiday/holiday.router.js
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-import express from "express";
-
-import { validateHoliday } from './holiday.validation.js';
-
-import { createHoliday, getHolidays, updateHoliday, deleteHoliday } from "./controller/holiday.controller.js";
-const router = express.Router();
-
-router.post("/", validateHoliday, createHoliday);
-router.get("/", getHolidays);
-router.put("/:id", validateHoliday, updateHoliday);
-router.delete("/:id", deleteHoliday);
-
-=======
 import { Router } from "express";
 import * as holidayController from "./controller/holiday.controller.js";
 import { validateCreateHoliday, validateUpdateHoliday, validateDeleteHoliday, validateGetHolidays } from "./holiday.validation.js";
@@ -26,5 +12,4 @@ router.route("/:id")
   .put(validateUpdateHoliday, holidayController.updateHoliday)
   .delete(validateDeleteHoliday, holidayController.deleteHoliday);
 
->>>>>>> Stashed changes
 export default router;
